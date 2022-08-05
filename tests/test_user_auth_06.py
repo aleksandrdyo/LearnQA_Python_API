9f680cec3cf80c8c78b0365e1bf32be5a4d00c64
dyo999 <dyoalex@gmail.com>
2022-08-05 23:35:58 +0300

    add framework

--- tests/test_user_auth_06.py
+++ tests/test_user_auth_06.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
 from lib.base_case import BaseCase
+from lib.assertions import Assertions
 
 class TestUserAuth(BaseCase):
     exclude_params = [
@@ -35,13 +36,17 @@ class TestUserAuth(BaseCase):
             cookies={"auth_sid":self.auth_sid}
         )
 
-        assert "user_id" in response2.json(), "There is no user id in the response"
-        user_id_from_check_method = response2.json()["user_id"]
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            self.user_id_from_auth_method,
+            "User id from auth method is not equal to user id from check method"
+        )
 
-        assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
-
-
-
+        # assert "user_id" in response2.json(), "There is no user id in the response"
+        # user_id_from_check_method = response2.json()["user_id"]
+        #
+        # assert self.user_id_from_auth_method == user_id_from_check_method, "User id from auth method is not equal to user id from check method"
 
     @pytest.mark.parametrize("condition", exclude_params)
     def test_negative_auth_check(self, condition):
@@ -51,9 +56,16 @@ class TestUserAuth(BaseCase):
         else:
             response2 = requests.get(self.link2, headers={"auth_sid": self.auth_sid})
 
-        assert "user_id" in response2.json(), "There is no user id in the second response"
+        Assertions.assert_json_value_by_name(
+            response2,
+            "user_id",
+            0,
+            f"User is authorized with condition {condition}"
+        )
 
-        user_id_from_check_method = response2.json()["user_id"]
-
-        assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
+        # assert "user_id" in response2.json(), "There is no user id in the second response"
+        #
+        # user_id_from_check_method = response2.json()["user_id"]
+        #
+        # assert user_id_from_check_method == 0, f"User is authorized with condition {condition}"
 
